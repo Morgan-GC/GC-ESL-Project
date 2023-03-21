f44c32c552b7e950b7ba661b6926ae0433727a0e
Morgan Steckley <Morgansteckley93@gmail.com>
2023-03-21 15:04:30 -0230

    next step in project, making a button that renders new button using HTMl and Javascript

--- JScode.js
+++ JScode.js
@@ -6,9 +6,10 @@ step 5: write functions that render button AND functions that delete buttons
 step 6: use functions from step 5 to make button dissapear and create new button ON CLICK (eventlistener)
 */
 
-function changeButtonText() 
-{
-    var elem = document.getElementById("myButton");
-    if (elem.value == "Test Button Unclicked") elem.value = "Test Button Clicked";
-    else elem.value = "Test Button Unclicked"
+function createButton() {
+    var myDiv = document.getElementById("newButton");    
+    var button = document.createElement('BUTTON');    
+    var text = document.createTextNode("create");     
+    button.appendChild(text);    
+    myDiv.appendChild(button); ;
     }
