865ec5f40cc4e1964bcf308ab448b24710efacbc
Morgan Steckley <Morgansteckley93@gmail.com>
2023-03-21 14:41:11 -0230

    added button with HTML and text change functionality with JS

--- JScode.js
+++ JScode.js
@@ -5,3 +5,10 @@ step 4: delete button using JS (or delete all)
 step 5: write functions that render button AND functions that delete buttons
 step 6: use functions from step 5 to make button dissapear and create new button ON CLICK (eventlistener)
 */
+
+function changeButtonText() 
+{
+    var elem = document.getElementById("myButton");
+    if (elem.value == "Test Button Unclicked") elem.value = "Test Button Clicked";
+    else elem.value = "Test Button Unclicked"
+    }
