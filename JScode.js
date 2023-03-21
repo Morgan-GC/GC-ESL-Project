17d441083f1a449f1ea3857bf6205015ea1a20b4
Morgan Steckley <Morgansteckley93@gmail.com>
2023-03-21 13:51:59 -0230

    first commit, still working out GIT

---
+++ JScode.js
@@ -0,0 +1,7 @@
+/*step 1: render button on screen using HTML
+step 2: change button text using JS
+step 3: render new button using JS (2 buttons, on with HTML other with JS)
+step 4: delete button using JS (or delete all)
+step 5: write functions that render button AND functions that delete buttons
+step 6: use functions from step 5 to make button dissapear and create new button ON CLICK (eventlistener)
+*/
