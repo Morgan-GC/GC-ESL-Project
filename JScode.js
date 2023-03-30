05453df2cf8d281f6bb5bd3e8ca1ecb1a63e31b4
Morgan Steckley <Morgansteckley93@gmail.com>
2023-03-30 18:02:56 -0230

    changed eventlistener to trigger on specific element rather whole document

--- JScode.js
+++ JScode.js
@@ -4,20 +4,41 @@ step 3: render new button using JS (2 buttons, on with HTML other with JS)
 step 4: delete button using JS (or delete all)
 step 5: write functions that render button AND functions that delete buttons
 step 6: use functions from step 5 to make button dissapear and create new button ON CLICK (eventlistener)
+what follows are further steps based on previous meeting with Mark, included here for reference
+Step 7: Make a button that when clicked generates a new list of buttons. This list of buttons is determined through a variable called arr. You can set the value of arr however way you want. but if for example it was ['button3','hello']. Then it would generate 2 new buttons 'button3' and 'hello'
+Step 8: Start with 3 different buttons and then make each of them generate a new list of buttons that is unique to each one. For example if i start the application with the following buttons
+Hey,
+hi buddy,
+Too busy today,
+
+Then we want each on of them to generate a different list of buttons based on which one i clicked. 
+
+Hey -> How are you, How is your day
+Hi buddy -> Hey my friend, oh i have missed you
+Too busy today -> sorry to hear
+
+Step 9: Create a system that uses objects to describe to our code the flow of buttons. This needs not to abide by any standard, as long as its easy to add more buttons that go to other buttons thats the goal. 
+
+
 */
 
+const element = document.getElementById("firstbutton");
+element.addEventListener("click",() => {createButton(); deleteButton()}, false);
 
-document.addEventListener("click",() => {createButton(); deleteButton()});
+var newButtonsArr = ['button1','button2','button3']; 
 
 function createButton() {
-    var myDiv = document.getElementById("newButton");    
+  
+    for (var i = 0; i < newButtonsArr.length; i++) { 
+
     var button = document.createElement('BUTTON');    
-    var text = document.createTextNode("create");     
+    var text = document.createTextNode(newButtonsArr[i]);     
     button.appendChild(text);    
-    myDiv.appendChild(button); 
+    document.body.appendChild(button); 
+    }
 };
 
  function deleteButton() {
-    document.getElementById("first_button").remove();
+    document.getElementById("firstbutton").remove();
  };
     
